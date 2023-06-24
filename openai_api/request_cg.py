743c13505f6ac4ea1d8b07acca389fa84869c172
Hiroo Takizawa <kei.cataract@gmail.com>
2023-06-24 21:13:29 +0900

    modify

--- openai_api/request_cg.py
+++ openai_api/request_cg.py
@@ -2,6 +2,9 @@ import os
 import sys
 import openai
 
+# import codecs
+
+# sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
 
 if __name__ == "__main__":
     message = sys.argv[1]
@@ -9,9 +12,8 @@ if __name__ == "__main__":
     openai.api_key = os.environ.get("API_KEY")
 
     completion = openai.ChatCompletion.create(
-        # model="gpt-3.5-turbo", 
         model="gpt-4",
         messages=[{"role": "user", "content": message}]
     )
 
-    print(completion)
+    print(completion.choices[0]["message"]["content"].strip())
